c443071d383ea5b7a902745705c36cd928b9340a
Ana Carolina R Lima <anacarolina.rdel@gmail.com>
2021-06-24 22:09:10 -0300

    agrupamento das classes num unico arquivo

--- js/game-controller.js
+++ js/game-controller.js
@@ -1,28 +1,119 @@
-class Controller {
+class Background {
     constructor(canvas, context) {
         this.canvas = canvas;
-        this.ctx = context;
-        this.background = new Background(this.canvas, this.ctx);
-        this.player = new Player(this.canvas, this.ctx, 350, 400, 240, 300);
-        this.score = document.querySelector(".game-score");
-        this.currentScore = this.score.innerText;
-    };
-    showScore() {
-        this.score.style.display = "block";
-    };
-    updateScore() {
-
-    };
-    startGame() {
-        this.updateCanvas();
-        this.showScore();
-        this.updateScore();
+        this.context = context;
+        this.posX = 0;
+        this.posY = 0;
+        const img = new Image();
+        img.onload = () => {
+            this.img = img;
+        };
+        img.src = './images/background.png';
     };
 
-    updateCanvas() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); 
-        this.background.draw();
-        this.player.draw();
-        requestAnimationFrame(this.updateCanvas.bind(this));
+    draw() {
+        this.context.globalAlpha = 0.4;
+        this.context.drawImage(this.img, this.posX, this.posY, this.canvas.width, this.canvas.height);
     };
+};
+
+class Player {
+    constructor(canvas, context, x, y, w, h) {
+        this.canvas = canvas;
+        this.context = context;
+        this.posX = x;
+        this.posY = y;
+        this.width = w;
+        this.height = h;
+        const img = new Image();
+        img.onload = () => {
+            this.img = img;
+        };
+        img.src = './images/kevin.png';
+        this.speed = 30;
+    }
+
+    draw() {
+        this.context.globalAlpha = 1;
+        this.context.drawImage(this.img, this.posX, this.posY, this.width, this.height);
+    };
+
+    moveRight() {
+        if (this.posX < this.canvas.width - this.width) {
+            this.posX += this.speed;
+        };
+    }
+
+    moveLeft() {
+        if (this.posX > 0) {
+        this.posX -= this.speed;
+        };
+    }
+
+    top() {
+        return this.posY;
+    }
+
+    bottom() {
+        return this.posY + this.height;
+    }
+
+    left() {
+        return this.posX;
+    }
+
+    right() {
+        return this.posX + this.width;
+    }
+
+    checkCollision(obstacle) {
+        if (this.posX < obstacle.posX + obstacle.width &&
+            this.posX + this.width > obstacle.posX &&
+            this.posY < obstacle.posY + obstacle.height &&
+            this.posY + this.height > obstacle.posY)
+        return true;
+    }
+};
+
+
+class Obstacle {
+    constructor(canvas, context, x, w, h) {
+        this.canvas = canvas;
+        this.context = context;
+        this.posX = x;
+        this.posY = 0;
+        this.width = w;
+        this.height = h;
+        const img = new Image();
+        img.onload = () => {
+            this.img = img;
+        };
+        img.src = './images/onion.png';
+        this.speed = 3;
+    };
+
+    draw() {
+        this.context.globalAlpha = 1;
+        this.context.drawImage(this.img, this.posX, this.posY, this.width, this.height);  
+    };
+
+    move() {
+        this.posY += this.speed;
+    }
+
+    top() {
+        return this.posY;
+    }
+
+    bottom() {
+        return this.posY + this.height;
+    }
+
+    left() {
+        return this.posX;
+    }
+
+    right() {
+        return this.posX + this.width;
+    }
 };
--- js/index.js
+++ js/index.js
@@ -1,31 +1,74 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-const startButton = document.getElementById('start-button');
-const newController = new Controller(canvas, context);
-
-
-if (startButton) {
-    startButton.addEventListener('click', event => {
-      newController.startGame();
-      startButton.innerText = 'Restart';
-    });
-};
-
-document.addEventListener('keydown', (e) => {
-  switch (e.code) {
-    case 'ArrowLeft':
-    case 'KeyA':
-      newController.player.speed -= 4;
-      break;
-    case 'ArrowRight':
-    case 'KeyD':
-      newController.player.speed += 4;
-      break;
+window.onload = () => {
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+  const background = new Background(canvas, context);
+  const player = new Player(canvas, context, 350, 400, 240, 300);
+  let animationId = null;
+  let frames = 0;
+  let obstacles = [];
+  let score = document.querySelector(".game-score");
+  
+  function showScore() {
+    score.style.display = "block";
   };
-  newController.player.move();
-  newController.updateCanvas();
-});
 
-document.addEventListener('keyup', (e) => {
-  newController.player.speed = 0;
-});
+  function startGame() {
+    updateCanvas();
+    showScore();
+  }
+
+  function createObstacle() {
+    const min = Math.ceil(90);
+    const max = Math.floor(910);
+    const obstacleX = Math.floor(Math.random() * (max - min)) + min;
+    obstacles.push(new Obstacle(canvas, context, obstacleX, 30, 30));
+  }
+
+  function updateObstacles() {
+    for (let i = 0; i < obstacles.length; i += 1) {
+      obstacles[i].move();
+      obstacles[i].draw();
+      if (obstacles[i].posY >= canvas.height) {
+        obstacles.shift();
+      }
+      if (player.checkCollision(obstacles[i])) {
+        console.log(obstacles[i].posX)
+      };
+    };
+    if (frames % 300 === 0) {
+      createObstacle();
+    }
+  }
+
+  function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function updateCanvas() {
+    frames += 1;
+    clearCanvas();
+    background.draw();
+    player.draw();
+    updateObstacles();
+    animationId = requestAnimationFrame(updateCanvas);
+  }
+
+  document.getElementById('start-button').onclick = () => {
+    startGame();
+    document.getElementById('start-button').innerText = 'Restart Game';
+  };
+
+  document.addEventListener('keydown', (event) => {
+    switch (event.code) {
+      case 'ArrowLeft':
+      case 'KeyA':
+        player.moveLeft();
+        break;
+      case 'ArrowRight':
+      case 'KeyD':
+        player.moveRight();
+        break;
+    };
+    updateCanvas();
+  });
+}
